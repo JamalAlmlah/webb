5f11990cdb04de9fb4644605c964b2b61a50f89a
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2019-01-16 11:09:36 +0100

    port done

---
+++ posts.js
@@ -0,0 +1,106 @@
+const postsData = [
+  {
+    title: 'Lorem Ipsum best not make any more threats to your website',
+    excerpt: `Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world has never seen. This placeholder text is gonna be HUGE. I have a 10 year old son. He has words. He is so good with these words it's unbelievable. If Trump Ipsum weren’t my own words, perhaps I’d be dating it.`,
+    content: `Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world has never seen. This placeholder text is gonna be HUGE. I have a 10 year old son. He has words. He is so good with these words it's unbelievable. If Trump Ipsum weren’t my own words, perhaps I’d be dating it.
+    Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. I don't think anybody knows it was Russia that wrote Lorem Ipsum, but I don't know, maybe it was. It could be Russia, but it could also be China. It could also be lots of other people. It also could be some wordsmith sitting on their bed that weights 400 pounds. Ok? I know words. I have the best words. An 'extremely credible source' has called my office and told me that Lorem Ipsum's birth certificate is a fraud. Some people have an ability to write placeholder text... It's an art you're basically born with. You either have it or you don't. You're telling the enemy exactly what you're going to do. No wonder you've been fighting Lorem Ipsum your entire adult life.
+    
+    The other thing with Lorem Ipsum is that you have to take out its family. Does everybody know that pig named Lorem Ipsum? She's a disgusting pig, right? Lorem Ipsum is FAKE TEXT! An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.
+    
+    That other text? Sadly, it’s no longer a 10. Lorem Ipsum's father was with Lee Harvey Oswald prior to Oswald's being, you know, shot.
+    
+    I think my strongest asset maybe by far is my temperament. I have a placeholding temperament. It’s about making placeholder text great again. That’s what people want, they want placeholder text to be great again.`,
+    writtenBy: 'Lorem Trump',
+    id: 1
+  },
+  {
+    title:
+      'Lorem ipsum dolor amet yr cardigan occupy letterpress ramps meggings',
+    excerpt:
+      'Lorem ipsum dolor amet yr cardigan occupy letterpress ramps meggings. Tilde four loko marfa keffiyeh jean shorts put a bird on it meh cray health goth. Microdosing cred copper mug, meditation pinterest YOLO fanny pack vaporware fingerstache subway tile vinyl venmo gochujang normcore lumbersexual. Ethical art party next level quinoa vegan farm-to-table austin pabst etsy pop-up hoodie yuccie helvetica godard tofu. Humblebrag wayfarers roof party trust fund chillwave.',
+    content: `Lorem ipsum dolor amet yr cardigan occupy letterpress ramps meggings. Tilde four loko marfa keffiyeh jean shorts put a bird on it meh cray health goth. Microdosing cred copper mug, meditation pinterest YOLO fanny pack vaporware fingerstache subway tile vinyl venmo gochujang normcore lumbersexual. Ethical art party next level quinoa vegan farm-to-table austin pabst etsy pop-up hoodie yuccie helvetica godard tofu. Humblebrag wayfarers roof party trust fund chillwave.
+    Fashion axe unicorn meggings freegan, fam irony offal actually jean shorts hell of fingerstache kitsch. Adaptogen whatever shaman slow-carb palo santo cred dreamcatcher vice, four dollar toast ethical. Vape ennui craft beer bushwick shoreditch. Locavore keytar wayfarers authentic.
+    
+    Cloud bread copper mug PBR&B selvage glossier prism salvia XOXO. Yr prism thundercats, typewriter tilde bicycle rights celiac intelligentsia gentrify. Franzen biodiesel small batch, salvia selfies readymade chartreuse messenger bag selvage tacos vice iceland gentrify paleo chambray. Cold-pressed succulents paleo, retro church-key air plant gentrify godard.
+    
+    Glossier single-origin coffee gastropub, humblebrag seitan williamsburg lyft slow-carb lumbersexual flexitarian pabst cronut ugh. Chicharrones vinyl mustache, tacos drinking vinegar prism helvetica paleo chillwave. Heirloom actually sustainable man bun pop-up whatever fanny pack bespoke schlitz XOXO shoreditch. Raw denim direct trade green juice, put a bird on it viral polaroid twee ramps etsy. XOXO whatever wayfarers, aesthetic pok pok schlitz asymmetrical man bun. Cray microdosing tote bag, beard locavore tacos PBR&B knausgaard church-key forage kale chips.
+    
+    Iceland four dollar toast meh listicle, subway tile vinyl salvia intelligentsia polaroid church-key thundercats. Shaman gentrify polaroid succulents swag, schlitz vexillologist bespoke farm-to-table photo booth hot chicken chicharrones. Franzen four dollar toast fingerstache jean shorts YOLO normcore. Migas 8-bit shabby chic four dollar toast chartreuse.
+    
+    Oh. You need a little dummy text for your mockup? How quaint.
+    
+    I bet you’re still using Bootstrap too…`,
+    writtenBy: 'Lorem Hipster',
+    id: 2
+  },
+  {
+    title: 'Apple didn’t invent anything this is why sucky ass',
+    excerpt:
+      'Apple didn’t invent anything this is why sucky ass, suddenly toys, and Apple copied Android’s notifications, who hype, prior to fact is, Apple are going down in the beginning blah, blah, blah, especially Gruber in my opinion fact is, I can get a better laptop for less because it didn’t even have copy and paste I think Jesus pad, in spite of Flash, moreover you suck.',
+    content: `Apple didn’t invent anything this is why sucky ass, suddenly toys, and Apple copied Android’s notifications, who hype, prior to fact is, Apple are going down in the beginning blah, blah, blah, especially Gruber in my opinion fact is, I can get a better laptop for less because it didn’t even have copy and paste I think Jesus pad, in spite of Flash, moreover you suck.
+    Android is better because it’s open, generally fanboi however I-Pod in conclusion Apple copied LG naturally typical fanboy whenever it’s open once brainwashed why notifications are way better on Android so as to you suck afterwards locked down.
+    
+    Fanboy, exactly because professional fanboy this includes cult of Steve, when moron thus crap.
+    
+    You don’t know anything, before fanboi while marketing until you’d buy shit if Apple sold it what Apple are nothing without Steve Jobs, on the contrary Android sells more phones, therefore fanboy, so fanboi to whom fanboy, hardly professional fanboy, on you suck because of death-grip, while Google Voice is better than Siri and TellMe put together.
+    
+    Cult of Jobs to ass-kissing on the one hand overpriced, first Antennagate another point is that hypnotised in addition Apple didn’t invent anything, to sum up Apple are nothing without Steve Jobs as well as Apple copied Android’s notifications overall fanboy personally death-grip, immediately I-Pod, where Flash which fanboi, at the end Antennagate on the whole fact is, I can get a better laptop for less on the other hand fanboi whose hypnotised not only you’d buy shit if Apple sold it since moron although Gruber.
+    
+    Blah, blah, blah besides fanboy, when you suck all in all cult of Jobs whereas it didn’t even have copy and paste furthermore crap for instance Jesus pad as a result toys in the end notifications are way better on Android in order that you suck that Android is better because it’s open, for this reason professional fanboy, owing to Google Voice is better than Siri and TellMe put together consequently marketing, whatever hype, such a professional fanboy, at first fanboi.
+    
+    Typical fanboy, what is more you don’t know anything, when Apple copied LG so that overpriced, above all you suck in the main locked down, at the beginning fanboy, I would say that sucky ass, then ass-kissing at last it’s open particularly Android sells more phones, but while brainwashed, in contrast fact is, Apple are going down.
+    
+    Cult of Steve, next cult of Jobs during fanboy so that marketing to begin with Google Voice is better than Siri and TellMe put together soon Apple didn’t invent anything, I believe professional fanboy but moron, but also fanboi, despite fanboy after that Antennagate finally you’d buy shit if Apple sold it, due to Apple copied Android’s notifications eventually cult of Steve, not enough sucky ass as soon as Apple are nothing without Steve Jobs after toys.
+    
+    Crap, for example Android is better because it’s open, apparently death-grip, as fanboy, for one thing Gruber, nevertheless you suck while fact is, Apple are going down naturally hypnotised at last blah, blah, blah, in contrast fanboi at the end typical fanboy on the other hand you don’t know anything, besides you suck, for this reason Apple copied LG, eventually you suck first hype apparently fact is, I can get a better laptop for less especially notifications are way better on Android and locked down all in all it’s open.
+    
+    Brainwashed, until I-Pod although fanboi next professional fanboy, prior to it didn’t even have copy and paste, this includes Jesus pad for example Flash, I believe overpriced, after ass-kissing, so that Android sells more phones at first you suck where you suck, nevertheless I-Pod, in the beginning Apple copied Android’s notifications in addition Android sells more phones for instance fanboy particularly fanboi, because marketing so that it’s open, afterwards you’d buy shit if Apple sold it, whenever Android is better because it’s open, then typical fanboy.
+    
+    Fact is, Apple are going down while sucky ass in order that blah, blah, blah in the main cult of Steve above all cult of Jobs as a result Gruber, for one thing hype, to sum up Jesus pad, owing to Flash, but while brainwashed, when death-grip due to fanboy overall Apple copied LG.
+    
+    You don’t know anything I think locked down when it didn’t even have copy and paste on the whole fanboi, not enough fanboy, this is why fanboi however Apple didn’t invent anything on the one hand fact is, I can get a better laptop for less, so hypnotised, consequently overpriced, at the beginning crap since toys who Google Voice is better than Siri and TellMe put together, as well as you suck not only Antennagate, another point is that notifications are way better on Android so as to professional fanboy, which moron, because of Apple are nothing without Steve Jobs therefore ass-kissing to begin with professional fanboy in my opinion fanboi suddenly fanboi, why fanboy.
+    
+    Fact is, I can get a better laptop for less in conclusion sucky ass, soon you don’t know anything, generally death-grip, that brainwashed despite Apple copied LG moreover Android sells more phones on fanboi.
+    
+    Jesus pad to professional fanboy, whose ass-kissing whereas locked down, once blah, blah, blah after that you’d buy shit if Apple sold it, such a Apple didn’t invent anything before overpriced, hardly hypnotised, but also Flash, during Google Voice is better than Siri and TellMe put together what is more I-Pod, on the contrary typical fanboy as soon as hype I would say that fanboy thus moron in the end marketing in spite of toys, as you suck.
+    
+    Notifications are way better on Android when it didn’t even have copy and paste, personally Antennagate furthermore you suck, but it’s open whatever fanboy, to whom fact is, Apple are going down what Android is better because it’s open exactly because you suck finally Gruber, immediately cult of Steve where cult of Jobs in the main crap so that professional fanboy.`,
+    writtenBy: 'The troll',
+    id: 3
+  },
+  {
+    title: 'Webtwo ipsum voxy odeo cotweet',
+    excerpt:
+      'Webtwo ipsum voxy odeo cotweet, xobni. Handango waze sifteo, kazaa.  Blekko joyent loopt, wesabe. Jumo bubbli heroku nuvvo eskobo, klout odeo chumby. Shopify vuvox groupon chartly, zoodles koofers meevee yammer, chartly wikia. voki dropio. plugg wakoopa. Cotweet zoosk wakoopa lijit, ngmoco prezi. Jabber kiko handango geni zoodles kno, twitter jajah boxbe. diigo. Ngmoco qeyno plugg octopart trulia, stypi convore reddit, dropio heroku zoodles. Movity zimbra wufoo palantir oooooc zynga, jabber disqus xobni edmodo. Heekya oooj sococo appjet, ning. Babblely fleck disqus mozy bubbli blyve yammer, wakoopa kiko zoodles twones hojoki.',
+    content: `Webtwo ipsum voxy odeo cotweet, xobni. Handango waze sifteo, kazaa.  Blekko joyent loopt, wesabe. Jumo bubbli heroku nuvvo eskobo, klout odeo chumby. Shopify vuvox groupon chartly, zoodles koofers meevee yammer, chartly wikia. voki dropio. plugg wakoopa. Cotweet zoosk wakoopa lijit, ngmoco prezi. Jabber kiko handango geni zoodles kno, twitter jajah boxbe. diigo. Ngmoco qeyno plugg octopart trulia, stypi convore reddit, dropio heroku zoodles. Movity zimbra wufoo palantir oooooc zynga, jabber disqus xobni edmodo. Heekya oooj sococo appjet, ning. Babblely fleck disqus mozy bubbli blyve yammer, wakoopa kiko zoodles twones hojoki.
+    Zanga ebay oooj lanyrd empressr plaxo ning, heekya yuntaa quora chegg. Zooomr blekko jumo prezi wesabe spotify, heekya insala chartly edmodo. Cuil joukuu scribd, jumo. Oooooc yammer kno gsnap grockit, blyve bubbli. Movity kippt eskobo bebo, zillow diigo. vimeo sifteo. Kno lanyrd quora weebly yoono foodzie chumby, knewton cloudera whrrl hipmunk. Boxbe mobly meevee gooru, dropio.
+    
+    Zillow bebo balihoo, heekya. Qeyno zoosk imvu imeem weebly joost zoho, omgpop reddit imvu zlio. Octopart flickr wakoopa twitter geni revver, jaiku lijit joyent. Napster zlio wesabe yoono waze loopt vimeo, quora zynga mobly kno. Foodzie plugg napster zooomr, foodzie dogster. Zlio unigo geni wufoo klout yammer, blekko unigo palantir sococo. Vuvox handango prezi groupon glogster reddit ebay, zinch zoosk zappos orkut. Kippt dropio geni voxy, chegg bubbli. Plaxo wufoo sclipo trulia airbnb kno, zooomr babblely qeyno etsy. Geni wikia weebly twones, babblely sclipo. Heroku jaiku gsnap cloudera plaxo, quora balihoo mozy gooru, trulia jajah geni.
+    
+    Tivo geni udemy kazaa, chumby omgpop. Lala jabber oovoo wesabe boxbe akismet blyve, chumby zappos waze lala. Lanyrd tivo qeyno disqus stypi kippt loopt, jibjab zimbra wufoo joyent kazaa. Sifteo etsy imeem omgpop sclipo jaiku, groupon zoosk ning. Boxbe wufoo dopplr sclipo trulia bitly jaiku, divvyshot stypi mzinga blippy.
+    
+    Zooomr empressr plaxo diigo fleck divvyshot, stypi heroku bitly. Sococo edmodo udemy kazaa ifttt zillow, movity woopra xobni. Lanyrd vuvox yammer elgg twones whrrl, prezi oovoo grockit. Kosmix palantir voxy bitly chumby, jiglu twitter. Handango chartly sococo, geni. Eskobo odeo zoho greplin hulu, jibjab odeo reddit. Knewton zynga jajah insala ebay, empressr trulia. groupon flickr reddit. Chumby wikia reddit grockit blippy, flickr grockit. Meevee doostang zappos spock tivo, scribd odeo. Glogster dopplr woopra insala heroku, divvyshot meevee heekya, chegg appjet akismet. ideeli. Twitter jabber bubbli tumblr plugg, bebo shopify xobni. Spock jibjab orkut zapier, blekko eduvant. Oooooc edmodo groupon, bubbli. Prezi zooomr empressr odeo wufoo plugg, akismet rovio mozy.
+    
+    `,
+    writtenBy: 'The Web',
+    id: 4
+  },
+  {
+    title: 'Our trials and triumphs became at once unique and universal',
+    excerpt: `Our trials and triumphs became at once unique and universal, black and more than black; in chronicling our journey, the stories and songs gave us a means to reclaim memories that we didn't need to feel shame about...memories that all people might study and cherish - and with which we could start to rebuild." This is where we are right now. It's because John McCain doesn't get it. So I have known Islam on three continents before coming to the region where it was first revealed. It's a story with a simple truth: that violence is a dead end. And it is no coincidence that countries where women are well-educated are far more likely to be prosperous.`,
+    content: `Our trials and triumphs became at once unique and universal, black and more than black; in chronicling our journey, the stories and songs gave us a means to reclaim memories that we didn't need to feel shame about...memories that all people might study and cherish - and with which we could start to rebuild." This is where we are right now. It's because John McCain doesn't get it. So I have known Islam on three continents before coming to the region where it was first revealed. It's a story with a simple truth: that violence is a dead end. And it is no coincidence that countries where women are well-educated are far more likely to be prosperous.
+    John Kerry believes in an America where all Americans can afford the same health coverage our politicians in Washington have for themselves. That is true genius of this nation. We can play Reverend Wright's sermons on every channel, every day and talk about them from now until the election, and make the only question in this campaign whether or not the American people think that I somehow believe or sympathize with his most offensive words. The fifth issue that we must address together is religious freedom.
+    
+    That experience guides my conviction that partnership between America and Islam must be based on what Islam is, not what it isn't. The victims were innocent men, women and children from America and many other nations who had done nothing to harm anybody.
+    
+    Louis, and thousands more like her, who has the grades, has the drive, has the will, but doesn't have the money to go to college. Islamic culture has given us majestic arches and soaring spires; timeless poetry and cherished music; elegant calligraphy and places of peaceful contemplation. No single nation should pick and choose which nations hold nuclear weapons.
+    
+    This was one of the tasks we set forth at the beginning of this campaign - to continue the long march of those who came before us, a march for a more just, more equal, more free, more caring and more prosperous America. She poured everything she had into me. And for the sake of our economy, our security, and the future of our planet, I will set a clear goal as President: in ten years, we will finally end our dependence on oil from the Middle East. I love this country, and so do you, and so does John McCain. It is based upon cultural and historical ties, and the recognition that the aspiration for a Jewish homeland is rooted in a tragic history that cannot be denied. But it is clear to all concerned that when it comes to nuclear weapons, we have reached a decisive point.
+    
+    God bless you.`,
+    writtenBy: 'Obama',
+    id: 5
+  }
+];
+
+module.exports = postsData;
--- server.js
+++ server.js
@@ -1,7 +1,36 @@
-const http = require('http');
-const httpServer = http.createServer((request, response) => {const route = request.url;
-  console.log(route);
+require("./posts");
+const http = require("http");
+const StringDecodeer = require("string_decoder").StringDecoder;
+
+const httpServer = http.createServer((request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Request-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headrs', 'OPTIONS, GET');
+  response.setHeader('OPTIONS', '*');
+  if (request.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+
+  let route = request.url;
+  const method = request.method;
+  let dynamicArguments = request.url.split("/");
+  route = dynamicArguments[1];
+  dynamicArguments = dynamicArguments.splice(2);
+  const headers = request.headers;
+  const decoder = new StringDecodeer("utf8");
+  let payload = "";
+  request.on("data", data => {
+    payload += decoder.end();
+  });
+  request.on("end", () => {
+    payload += decoder.end();
+  });
+  if (route === "forum" && method === "GET") {
+    response.setHeader("Conent-Type", "application/json");
+    response.writeHead(200);
+    response.end(JSON.stringify({ post: "En forum post" }));
+  }
 });
-httpServer.listen(1337, () => console.log('webbserver startad på port 1337'));
-
-
+httpServer.listen(8000, () => console.log("webbserver startad på port 8000"));
